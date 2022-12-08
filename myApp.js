82ad04ec6526f58c323d95a15ab9f5370bbd8d6f
Mark Sayers <marksayers46@icloud.com>
2022-12-08 15:03:42 -0600

    setting HTML server & express.static to serve static assets

--- myApp.js
+++ myApp.js
@@ -1,10 +1,17 @@
 let express = require('express');
 let app = express();
 
+// app.get("/", function(req, res) {
+//     res.send("Hello Express")
+// });
 
-app.get("/", function(req, res) {
-    res.send("Hello Express")
-});
+// Serve HTML file (HTML Server)
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
 
- 
+// Serve Static Assets (stylesheets, scripts, images)
+app.use("/public", express.static(__dirname + "/public"))
+
+
 module.exports = app;
